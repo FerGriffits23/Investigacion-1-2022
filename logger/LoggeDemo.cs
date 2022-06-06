f530141f678d85273314c7fa873bf6db67f88d8c
unknown <Fernando.Griffits8@ujcv.edu.hn>
2022-06-05 21:02:18 -0600

    trabajo de investigacion 1 2022

---
+++ logger/LoggeDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace logger
+{
+    public class LoggeDemo
+    {
+
+        private string path = "";
+        private string brinco = "\n";
+
+        public LoggeDemo(string path)
+        {
+            this.path = path;
+        }
+
+        public void Add(string sLog)
+        {
+            CreateDirectory();
+            string nombre = GetNameFile();
+            string cadena = "";
+
+            cadena += DateTime.Now + " - " + sLog + Environment.NewLine;
+
+            StreamWriter sw = new StreamWriter(path + "/" + nombre, true);
+            sw.Write(cadena);
+            sw.Close();
+
+        }
+
+        #region HELPER
+        private string GetNameFile()
+        {
+            string nombre = "";
+
+            nombre = "log_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".log";
+
+            return nombre;
+        }
+
+        private void CreateDirectory()
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+        }
+        #endregion
+    }
+
+}
+    
+
---
+++ logger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggeDemo log = new LoggeDemo(@"C:\Users\Fernando\source\repos\logger\logger\");
+            log.Add("ocurrion un error");
+        }
+    }
+}
